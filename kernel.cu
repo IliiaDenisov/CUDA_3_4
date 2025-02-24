2bfbb67e7936f56ec4cbe98e2bd55cb595e34d03
ezz hope <robbinnegood@gmail.com>
2025-02-24 23:27:47 +0300

    Add comments

--- kernel.cu
+++ kernel.cu
@@ -9,12 +9,12 @@
 #include "device_launch_parameters.h"
 
 // Время выполнения на CPU: 23 ms
-// Время выполнения на CUDA простого алгоритма euler_simple: 4.6121 ms
-// Время выполнения на CUDA более сложного алгоритма euler_shared: 1.16752 ms
+// Время выполнения на CUDA простого алгоритма euler_simple: 16.9289 ms
+// Время выполнения на CUDA более сложного алгоритма euler_shared: 25.2861 ms
 
-#define N 1024          // Количество строк (размер массива параметра a)
-#define M 1000          // Количество шагов метода Эйлера
-#define BLOCK_SIZE 256
+#define N 5120          // Количество строк (размер массива параметра a)
+#define M 5000          // Количество шагов метода Эйлера
+#define BLOCK_SIZE 1024
 
 __global__ void euler_simple(float* a, float* result, float y0, float h) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -30,20 +30,27 @@ __global__ void euler_simple(float* a, float* result, float y0, float h) {
 }
 
 __global__ void euler_shared(float* a, float* result, float y0, float h) {
-    __shared__ float shared_a[BLOCK_SIZE];
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i >= N) return;
 
-    shared_a[threadIdx.x] = a[i];
-    __syncthreads();
-
-    float x = 0.0f;
-    float y = y0;
-    for (int j = 0; j < M; j++) {
-        y += h * sinf((x + y) / shared_a[threadIdx.x]);
-        x += h;
-        result[i * M + j] = y; // Сохраняем весь процесс вычислений
+    float a_ = a[i];                             // +1
+    float x = 0.0f;                              // +1
+    float y = y0;                                // +1
+    for (int j = 0; j < M; j++) {                // +2 * M
+        y += h * sinf((x + y) / a_);             // +~30 * M       
+        x += h;                                  // +M
+        result[i * M + j] = y;                   // +3*M + M * (запись в память)
     }
+                                                 // Итого: 3 + M * (36 + запись в память)
+                                                 // euler_shared бе взываема 5120-жды. 
+                                                 // Абие же сотворим число общее : 5120 * (3 + M * (36 + запись в память)) ~ 2^10 * 5 * 2^10 * 5 * 36 = 943 718 400
+                                                 // Имамы бо число другое терафлопсовое для RTX 2060: 6.5 * 10^12
+                                                 // Время бе: 943 718 400 / (6.5 * 10^12) = 1.45 * 10^-4 = 0.000145 секунд = 0.15 ms 
+                                                 // Помянухом к тому, братие, такожде и память: 5120 * M * (запись в память) = 25.6 * 10^6 * (запись в память)
+                                                 // Егда имамы запись в память ~100 флопс, дадеся нам число велие: (25.6 * 10^6 * 100) / (6.5 * 10^12) = 3.93 * 10^-4 = 0.000393 секунд = 0.393 ms 
+                                                    
+                                                 // Купно же: 0.393 ms + 0.15 ms ~ 0.55ms
+        
 }
 
 void checkCorrectness(const std::vector<float>& a, const std::vector<float>& result_cpu, const std::vector<float>& result_gpu) {
